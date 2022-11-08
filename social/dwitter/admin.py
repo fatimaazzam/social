f3ef26c2f4224b40b1a95eaf1cfa5646e3879a6e
fatima <fatimaazzam1@gmail.com>
2022-11-08 20:00:47 +0200

    Lecture 8-11-2022

--- social/dwitter/admin.py
+++ social/dwitter/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from .models import Profile
-admin.site.register(Profile)
+#admin.site.register(Profile)
 #admin.site.unregister(Group)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
 
 
 class UserAdmin(admin.ModelAdmin):
     model = User
     # Only display the "username" field
     fields = ["username"]
+    inlines = [ProfileInline]
+
 # Register your models here.
+
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
